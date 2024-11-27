8e3edf24a8b763890b28dfeddcdb1a72c873d307
Diego Vaccher <dvaccher@estilos.it>
2024-11-27 08:58:57 +0100

    Test commit

--- src/EntryPoint.cpp
+++ src/EntryPoint.cpp
@@ -6,7 +6,7 @@ int main()
 	trx::core::Application::Get().Initialize();
 
 	//trx::core::Application::Get().Run();
-	// Test Commit
+	// Test Commit 2
 
 	trx::core::Application::Get().Shutdown();
 	return 0;
